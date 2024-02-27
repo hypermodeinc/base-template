fb1b0c203d788c5c79ebdcd7d8a8f341dda77f23
Matt Johnson-Pint <mjp@hypermode.com>
2024-02-27 11:34:45 -0800

    Hardcode model id

--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -1,7 +1,9 @@
 import { JSON } from "json-as";
 import { model } from "@hypermode/functions-as";
 
-export function testClassifier(modelId: string, text: string): string {
+const modelId = "msp-523a8138-76b3-406f-9386-c53aa9b23c44";
+
+export function testClassifier(text: string): string {
   const results = model.classifyText(modelId, text);
   return JSON.stringify(results);
 }
