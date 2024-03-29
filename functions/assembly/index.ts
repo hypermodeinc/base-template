0c6a755477eb21b3d68abd4d5de0e724c15f53f7
Ryan Fox-Tyler <60440289+ryanfoxtyler@users.noreply.github.com>
2024-03-29 17:19:50 -0400

    Update index.ts

--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -1,8 +1,8 @@
 import { JSON } from "json-as";
-import { inference } from "@hypermode/functions-as";
+import { model } from "@hypermode/functions-as";
 
 export function testClassifier(text: string): string {
   const modelName = "sentiment-classifier";
-  const results = inference.computeClassificationLabels(modelName, text);
+  const results = model.classifyText(modelName, text);
   return JSON.stringify(results);
 }
