9a2e92bd5da1974dbaec42e674eb7dc09ec977f7
Ryan Fox-Tyler <60440289+ryanfoxtyler@users.noreply.github.com>
2024-03-29 17:21:24 -0400

    Update Template for Public Release (#9)

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# sample-project
-Sample Hypermode Project
+This is a template for new Hypermode projects. For more information on project configuration, [reference the docs](https://docs.hypermode.com/manifest).
--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -1,9 +1,8 @@
 import { JSON } from "json-as";
 import { model } from "@hypermode/functions-as";
 
-const modelId = "msp-523a8138-76b3-406f-9386-c53aa9b23c44";
-
 export function testClassifier(text: string): string {
-  const results = model.classifyText(modelId, text);
+  const modelName = "sentiment-classifier";
+  const results = model.classifyText(modelName, text);
   return JSON.stringify(results);
 }
