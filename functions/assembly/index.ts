aab6961e801142e8c2cc61119c658bc8f3923827
Matt Johnson-Pint <mjp@hypermode.com>
2024-04-25 01:41:25 -0700

    Update for 0.6.0 (#21)

--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -1,6 +1,7 @@
-import { model, ClassificationResult } from "@hypermode/functions-as";
+import { inference } from "@hypermode/functions-as";
 
-export function testClassifier(text: string): ClassificationResult {
-  const modelName = "sentiment-classifier";
-  return model.classifyText(modelName, text);
+export function testClassifier(text: string): string {
+  const modelName = "sentiment_classifier";
+  const threshold: f32 = 0.5;
+  return inference.classifyText(modelName, text, threshold);
 }
