c6a8f53284e9413a859c1a11cce2bb091e1a5598
Matt Johnson-Pint <mjp@hypermode.com>
2024-05-08 09:25:05 -0700

    Update manifest to use hyphens

--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -1,7 +1,7 @@
 import { inference } from "@hypermode/functions-as";
 
 export function testClassifier(text: string): string {
-  const modelName = "sentiment_classifier";
+  const modelName = "sentiment-classifier";
   const threshold: f32 = 0.5;
   return inference.classifyText(modelName, text, threshold);
 }
