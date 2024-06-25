52f5b7cdeb390166f674ba6fe8ada03c66f8c073
Matt Johnson-Pint <mjp@hypermode.com>
2024-06-24 22:18:00 -0700

    Update classifier example

--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -1,7 +1,18 @@
-import { inference } from "@hypermode/functions-as";
+import { models } from "@hypermode/functions-as";
+import { ClassificationModel } from "@hypermode/models-as/models/experimental/classification";
+
+const modelName = "sentiment-classifier";
+const threshold: f32 = 0.5;
 
 export function testClassifier(text: string): string {
-  const modelName = "sentiment-classifier";
-  const threshold: f32 = 0.5;
-  return inference.classifyText(modelName, text, threshold);
+  const model = models.getModel<ClassificationModel>(modelName);
+  const input = model.createInput([text]);
+  const output = model.invoke(input);
+
+  const prediction = output.predictions[0];
+  if (prediction.confidence >= threshold) {
+    return prediction.label;
+  }
+
+  return "";
 }
